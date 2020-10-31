99af13516bed21610b0c36be827632800b710cba
ebakoyan <69166469+ebakoyan@users.noreply.github.com>
2020-10-31 04:20:35 +0400

    s

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ listIMG= [];
 const BlockSize=20;
 const IMGSize=10;
 for(i=0;i<20;i++){
-    listBlock[i]=i+1;
+    listBlock[i]=i;
     if(i<=9){
         listIMG[i]=i+1;
     }
@@ -23,13 +23,13 @@ function shuffle(array) {
   shuffle(listBlock);
   shuffle(listIMG);
   fruit= document.getElementsByClassName("fruit");
-  for(i=0,j=19;i<10;i++,j--){
-    a=listIMG[i];
-    str="./img/";
-    str2=String(a);
-    str3=".png"
-    res=str.concat(str2);
-    res=res.concat(str3);
-    fruit[listBlock[i]].getElementsByTagName("img")[0].setAttribute("src",res);  
-    fruit[listBlock[j]].getElementsByTagName("img")[0].setAttribute("src",res);  
-}
+//   for(i=0,j=19;i<10;i++,j--){
+//     a=listIMG[i];
+//     str="./img/";
+//     str2=String(a);
+//     str3=".png"
+//     res=str.concat(str2);
+//     res=res.concat(str3);
+//     fruit[listBlock[i]].getElementsByTagName("img")[0].setAttribute("src",res);  
+//     fruit[listBlock[j]].getElementsByTagName("img")[0].setAttribute("src",res);  
+// }
