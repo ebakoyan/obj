81d60b3b5db514dab36f9c1a09105b4f73306ff2
ebakoyan <69166469+ebakoyan@users.noreply.github.com>
2020-10-31 21:23:38 +0400

    a

--- js/main.js
+++ js/main.js
@@ -33,3 +33,41 @@ function shuffle(array) {
     fruit[listBlock[i]].getElementsByTagName("img")[0].setAttribute("src",res);  
     fruit[listBlock[j]].getElementsByTagName("img")[0].setAttribute("src",res);  
 }
+block1=0,block2=1;
+s=0;
+function test(a,b){
+  if(a.src===b.src){
+    b.style.display="none";
+    a.style.display="none";
+  }
+  else return;
+}
+function Game(block){
+  s++;
+  block=block || window.event;
+  block=block.target || block.srcElement;
+  block.style.zIndex="600";
+  if(s==1){
+  block1=block;
+  }
+  else if(s==2){
+    block2=block;
+    test(block1,block2);
+    s=0;
+  }
+}
+function timeFunction() {
+  setTimeout(function(){
+    for(i=0;i<fruit.length;i++){
+      fruit[i].getElementsByTagName("img")[0].style.zIndex = "0";
+} }, 2000);
+}
+function Play(){
+  document.querySelector(".play").style.display="none";
+  document.querySelector(".gameBlock").style.display="grid";
+  timeFunction()
+}
+
+
+
+  
